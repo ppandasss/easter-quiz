3b15a2f0d5740f431e199f83f39461f8540deca9
ppandasss <praewpanda97@gmail.com>
2025-04-17 09:55:37 +0700

    Import Question Pictures + Test

--- data.js
+++ data.js
@@ -2,7 +2,7 @@
 const questions = [
     {
         text: "Now it's 3AM and you're still working at your computer. \n You look at the pile of soon-due assignments that is...",
-        image: "images/question-placeholder.png",
+        image: "images/Questions/01.png",
         answers: [
             { text: "Many", types: [] },
             { text: "So many", types: [] },
@@ -12,7 +12,7 @@ const questions = [
     {
         // B
         text: "You're getting sleepy, but suddenly\u2014a bunny hops out from your screen!\n\n\"Oh, hi! Dark corner eyes... unsubmitted assignments... and back pain! You're perfect, come with me!\" The bunny grabs your hand and pulls you toward the screen.",
-        image: "images/question-placeholder.png",
+        image: "images/Questions/02.png",
         answers: [
             { text: "Did I fall asleep? Am I hallucinating from too much caffeine?", types: ["S"] },
             { text: "Yay! Magic is real! What am I gonna isekai into?", types: ["N"] },
@@ -21,7 +21,7 @@ const questions = [
     {
         // J
         text: "\"Hi, human. My name's Aster. I'm taking you to join the Easter egg hunt. The winner gets a wish from the goddess this year!\"\n\nWhat first comes to your mind is...",
-        image: "images/question-placeholder.png",
+        image: "images/Questions/03.png",
         answers: [
             { text: "An egg hunt? Is my body ready for that right now\u2014am I wearing any shoes?", types: ["S"] },
             { text: "Any wish? What should I ask for? I wonder who that goddess is.", types: ["N"] },
@@ -32,12 +32,12 @@ const questions = [
         type: "shortAnswer",
         id: "relaxMethod",
         text: "You realize you have unfinished assignments, and you're unsure if you should go.\n\n\"Worried about that? Nah\u2014worry about your back! You've got to get moving. I know you've been tired lately. All work and no rest isn't good for you. Treat this as a break!\n\nTell me\u2014what do you do when it all feels like too much?\"",
-        image: "images/question-placeholder.png",
+        image: "images/Questions/04.png",
         answers: null
     },
     {
         text: "\"That sounds nice! This egg hunt's a little different, but I promise it'll be fun!\n\nI want you on my team because...you've got something special that'll help me win!\nAre you in? If we win, I'll share the wish with you.\"",
-        image: "images/question-placeholder.png",
+        image: "images/Questions/05.png",
         answers: [
             { text: "Alright, I'm in!", types: [] },
         ]
@@ -45,7 +45,7 @@ const questions = [
     {
         // D
         text: "You and Aster arrive in a magical forest. Bunnies are running in all directions.\n\n\"The hunt has already started\u2014we're late! Follow me!\"",
-        image: "images/question-placeholder.png",
+        image: "images/Questions/06.png",
         answers: [
             { text: "Wait! Shouldn't we plan our moves first?", types: ["J"] },
             { text: "Run run run! There must be eggs somewhere!", types: ["P"] },
@@ -54,7 +54,7 @@ const questions = [
     {
         // A
         text: "While collecting eggs in the first area, Aster says\n\n\"We actually missed the team-forming earlier... we could've had more bunnies join us.\"",
-        image: "images/question-placeholder.png",
+        image: "images/Questions/07.png",
         answers: [
             { text: "Nah, just us is good. Small group = less chaos!", types: ["I"] },
             { text: "A big team sounds better. More people = more fun!", types: ["E"] },
